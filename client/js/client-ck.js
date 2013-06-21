d070589c83758fe522786d8c9968df356b58438f
Matt Benton <matt@mattbenton.net>
2013-06-21 00:24:16 -0700

    Added game loop and row deletion

--- client/js/client-ck.js
+++ client/js/client-ck.js
@@ -1,4 +1,36 @@
+(function() {
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+    window.cancelRequestAnimationFrame = window[vendors[x]+
+      'CancelRequestAnimationFrame'];
+  }
+
+  if ( !window.requestAnimationFrame ) {
+    window.requestAnimationFrame = function(callback) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+        timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  }
+
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function(id) {
+      clearTimeout(id);
+    };
+  }
+}());
+
+/* **********************************************
+     Begin blocks.js
+********************************************** */
+
 /* jshint devel:true, unused:false */
+/* @codekit-prepend 'requestAnimationFrame.js' */
 
 (function(){
 
@@ -10,18 +42,46 @@
   var Tetris = root.Tetris = {};
 
   var Game = Tetris.Game = function () {
-    this.input = new Input(this);
+    var input = this.input = new Input(this);
 
-    this.grid = new Grid(10, 10);
-    this.grid.debug = true;
-    this.grid.render();
+    var grid = this.grid = new Grid(10, 15);
+    // grid.debug = true;
 
-    this.currentBlock = new Block();
-    this.currentBlock.randomize();
-    this.grid.blocks.push(this.currentBlock);
+    var block = this.currentBlock = new Block();
+    block.randomize();
 
-    this.grid.render();
-    this.grid.print();
+    var renderer = this.renderer = new Renderer(grid);
+    renderer.render();
+    renderer.renderBlock(block);
+
+    this.loop();
+  };
+
+  Game.prototype.loop = function () {
+    var self  = this;
+    var grid  = this.grid;
+    var block = this.currentBlock;
+
+    if ( block ) {
+      if ( grid.hitTestBlock(block, block.x, block.y + 1) ) {
+        grid.writeBlock(block, block.color.index);
+
+        block = new Block();
+        block.randomize();
+        this.currentBlock = block;
+      } else {
+        block.y++;
+      }
+    }
+
+    grid.checkCompletedRows();
+
+    this.renderer.render();
+    this.renderer.renderBlock(block);
+
+    setTimeout(function() {
+      self.loop();
+    }, 500);
   };
 
   Game.prototype.update = function () {
@@ -39,12 +99,20 @@
           break;
         case Input.UP:
           y--;
+          // block.rotate(1);
+          // if ( grid.hitTestBlock(block) ) {
+          //   block.rotate(-1);
+          // }
           break;
         case Input.RIGHT:
           x++;
           break;
         case Input.DOWN:
           y++;
+          // block.rotate(-1);
+          // if ( grid.hitTestBlock(block) ) {
+          //   block.rotate(1);
+          // }
           break;
         case Input.ROTATE:
           block.rotate(1);
@@ -53,7 +121,7 @@
           }
           break;
         case Input.STAMP:
-          grid.writeBlock(block, 1);
+          grid.writeBlock(block, block.color.index);
 
           this.currentBlock = block = new Block();
           block.randomize();
@@ -64,10 +132,16 @@
 
       if ( !grid.hitTestBlock(block, x, y) ) {
         block.x = x;
-        block.y = y;
+
+        if ( y > block.y ) {
+          block.y = y;
+        }
       }
 
-      grid.render();
+      // grid.render();
+      this.renderer.render();
+
+      this.renderer.renderBlock(block);
     }
   };
 
@@ -96,10 +170,53 @@
     $(window).on('keydown keyup', function ( event ) {
       self.handleKeyboard(event);
     });
+
+    // $(window).on('touchstart touchmove touchend', function (event) {
+    //   self.handleTouch(event);
+    // });
   };
 
   Input.prototype.removeListeners = function () {
     $(window).off('keydown keyup');
+  };
+
+  Input.prototype.handleTouch = function ( event ) {
+    event.preventDefault();
+
+    var game  = this.game;
+    var block = game.currentBlock;
+
+    if ( !block ) {
+      return;
+    }
+
+    if ( event.type === 'touchend' ) {
+    } else {
+      var touch = event.originalEvent.touches[0];
+      var x = touch.pageX;
+
+      if ( event.type === 'touchstart' ) {
+        this.startX = x;
+        this.lastX = x;
+      }
+
+      var dx = Math.round((x - this.startX) / game.grid.size / 2);
+
+      console.log(game.grid.hitTestBlockWithOffset(block, dx, 0));
+
+      if ( !game.grid.hitTestBlockWithOffset(block, dx, 0) ) {
+        block.x += dx;
+        // block.y = y;
+      }
+
+      window.requestAnimationFrame(function() {
+        game.grid.render();
+      });
+
+      // console.log(dx);
+
+      // this.lastX = x;
+    }
   };
 
   Input.prototype.handleKeyboard = function ( event ) {
@@ -126,71 +243,25 @@
 
   // The base **Renderer** class.
 
-  var Renderer = Tetris.Renderer = function () {
-
+  var Renderer = Tetris.Renderer = function ( grid ) {
+    this.grid = grid;
+    this.size = 25;
+    this.context = $('#grid')[0].getContext('2d');
   };
 
   Renderer.prototype.render = function () {
-
-  };
-
-  // # Color
-
-  // Defines the colors each block can be and provides a convenience method to
-  // get a random color.
-
-  var Color = Tetris.Color = {
-    red:    { fill: '#f14d43', stroke: '#7c1413' },
-    orange: { fill: '#f79210', stroke: '#c04207' },
-    yellow: { fill: '#ffd101', stroke: '#d77e06' },
-    green:  { fill: '#13cb15', stroke: '#067a0b' },
-    blue:   { fill: '#36abfb', stroke: '#2e72d5' },
-    purple: { fill: '#c390db', stroke: '#894ab5' }
-  };
-
-  Color.names = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
-
-  Color.random = function () {
-    return Color[Color.names[Math.floor(Math.random() * 6)]];
-  };
-
-  // # Tetris.Grid
-  //
-  //
-  var Grid = Tetris.Grid = function ( cols, rows ) {
-    // If enabled, `print()` will be called each time the collision data
-    // changes.
-    this.debug = false;
-
-    this.size = 25;
-
-    this.blocks = [];
-
-    this.$grid = $('#grid');
-    this.context = this.$grid[0].getContext('2d');
-
-    this.clear(cols, rows);
-  };
-
-  Grid.prototype.render = function () {
     var ctx  = this.context;
-    var grid = this.grid;
-    var cols = this.cols;
-    var rows = this.rows;
     var size = this.size;
-    var x, y, bit;
+    var grid = this.grid;
+    var cols = grid.cols;
+    var rows = grid.rows;
+    var x, y, bit, color;
 
-    var blocks = this.blocks;
-    var blockCount = blocks.length;
-    var i, block, color, shape, shapeSize, shapeData;
-
+    // Clear everything.
     ctx.clearRect(0, 0, cols * size, rows * size);
 
     // Render grid lines.
     ctx.save();
-    // Canvas calculates stuff on the half pixel, so offset everything by half
-    // a pixel so we actually get a line one pixel wide.
-    // ctx.translate(0.5, 0.5);
     for ( y = 0; y < rows + 1; y++ ) {
       for ( x = 0; x < cols + 1; x++ ) {
         ctx.moveTo(x * size, 0);
@@ -205,35 +276,142 @@
     ctx.stroke();
     ctx.restore();
 
+    // Render grid bits.
     ctx.save();
-    // ctx.translate(0.5, 0.5);
-    for ( i = 0; i < blockCount; i++ ) {
-      block = blocks[i];
-      shape = block.shape;
-      shapeData = shape.data;
-      shapeSize = shape.size;
+    ctx.lineWidth = 2;
+    for ( y = 0; y < rows; y++ ) {
+      for ( x = 0; x < cols; x++ ) {
+        bit = grid.grid[y][x];
+        if ( bit ) {
+          color = Color[bit];
+          ctx.fillStyle   = color.fill;
+          ctx.strokeStyle = color.stroke;
+          ctx.beginPath();
 
-      ctx.fillStyle   = block.color.fill;
-      ctx.strokeStyle = block.color.stroke;
-      ctx.lineWidth   = 2;
-      ctx.beginPath();
+          ctx.rect(x * size, y * size, size, size);
 
-      for ( y = 0; y < shapeSize; y++ ) {
-        for ( x = 0; x < shapeSize; x++ ) {
-          bit = shapeData[y][x];
-          if ( bit ) {
-            ctx.rect((x + block.x) * size, (y + block.y) * size, size, size);
-          }
+          ctx.closePath();
+          ctx.fill();
+          ctx.stroke();
         }
       }
-
-      ctx.closePath();
-      ctx.fill();
-      ctx.stroke();
     }
     ctx.restore();
   };
 
+  Renderer.prototype.renderBlock = function ( block ) {
+    var ctx       = this.context;
+    var size      = this.size;
+    var shapeData = block.shape.data;
+    var shapeSize = block.shape.size;
+    var x, y, bit;
+
+    ctx.save();
+    ctx.fillStyle   = block.color.fill;
+    ctx.strokeStyle = block.color.stroke;
+    ctx.lineWidth   = 2;
+    ctx.beginPath();
+
+    for ( y = 0; y < shapeSize; y++ ) {
+      for ( x = 0; x < shapeSize; x++ ) {
+        bit = shapeData[y][x];
+        if ( bit ) {
+          ctx.rect((x + block.x) * size, (y + block.y) * size, size, size);
+        }
+      }
+    }
+
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
+  };
+
+  // # Color
+
+  // Defines the colors each block can be and provides a convenience method to
+  // get a random color.
+
+  var Color = Tetris.Color = {
+    1: { index: 1, fill: '#f14d43', stroke: '#7c1413' }, // Red
+    2: { index: 2, fill: '#f79210', stroke: '#c04207' }, // orange
+    3: { index: 3, fill: '#ffd101', stroke: '#d77e06' }, // Yellow
+    4: { index: 4, fill: '#13cb15', stroke: '#067a0b' }, // Green
+    5: { index: 5, fill: '#36abfb', stroke: '#2e72d5' }, // Blue
+    6: { index: 6, fill: '#c390db', stroke: '#894ab5' },  // Purple
+    7: { index: 7, fill: '#565656', stroke: '#2d2d2d' }  // Charcoal (debugging)
+  };
+
+  Color.RED    = 1;
+  Color.ORANGE = 2;
+  Color.YELLOW = 3;
+  Color.GREEN  = 4;
+  Color.BLUE   = 5;
+  Color.PURPLE = 6;
+  Color.DEBUG  = 7;
+
+  // Color.names = { red: 1, orange: 2, yellow: 3, green: 4, blue: 5, purple: 6 };
+
+  Color.random = function () {
+    return Color[Math.floor(Math.random() * 6) + 1];
+  };
+
+  // # Tetris.Grid
+  //
+  //
+  var Grid = Tetris.Grid = function ( cols, rows ) {
+    // If enabled, `print()` will be called each time the collision data
+    // changes.
+    this.debug = false;
+    this.clear(cols, rows);
+  };
+
+  // Checks the grid for any solid rows and deletes them.
+  Grid.prototype.checkCompletedRows = function () {
+    var grid = this.grid;
+    var cols = this.cols;
+    var rows = this.rows;
+    var x, y, bit, isValid;
+
+    for ( y = 0; y < rows; y++ ) {
+      isValid = true;
+
+      for ( x = 0; x < cols; x++ ) {
+        bit = grid[y][x];
+        if ( !bit ) {
+          isValid = false;
+          break;
+        }
+      }
+
+      if ( isValid ) {
+        // for ( x = 0; x < cols; x++ ) {
+        //   grid[y][x] = Color.DEBUG;
+        // }
+        this.deleteRow(y);
+        return;
+      }
+    }
+  };
+
+  // Deletes a row and shifts all bits above it downwards to fill the empty
+  // space.
+  Grid.prototype.deleteRow = function ( rowIndex ) {
+    var grid = this.grid;
+    var cols = this.cols;
+    var rows = this.rows;
+    var x, y, bit, isValid;
+
+    for ( y = rowIndex; y > 0; y-- ) {
+      for ( x = 0; x < cols; x++ ) {
+        if ( y - 1 > 0 ) {
+          grid[y][x] = grid[y-1][x];
+        }
+      }
+    }
+  };
+
+  // Clears the grid and optionally resizes it.
   Grid.prototype.clear = function ( cols, rows ) {
     var x, y, row;
 
@@ -244,28 +422,10 @@
     this.rows = rows;
 
     this.grid = create2dArray(cols, rows);
-    // this.fillBorder();
   };
 
-  Grid.prototype.fillBorder = function ( value ) {
-    var grid = this.grid;
-    var cols = this.cols;
-    var rows = this.rows;
-    var i;
-
-    value = value || 9;
-
-    for ( i = 0; i < cols; i++ ) {
-      grid[0][i]        = value;
-      grid[rows - 1][i] = value;
-    }
-
-    for ( i = 0; i < rows; i++ ) {
-      grid[i][0]        = value;
-      grid[i][rows - 1] = value;
-    }
-  };
-
+  // Prints a textual representation of the current state of the grid to the
+  // console.
   Grid.prototype.print = function () {
     if ( console.clear && 0 ) {
       console.clear();
